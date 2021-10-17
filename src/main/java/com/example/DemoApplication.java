1878427abf8effd07a6610298439ced0a1673bd2
liuhuanhuan963019 <2432783449@qq.com>
2021-10-17 16:39:02 +0800

    first commit

---
+++ src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 徐森威
+ */
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/entity/Test.java
@@ -0,0 +1,117 @@
+package com.example.entity;
+
+import com.gitee.sunchenbin.mybatis.actable.annotation.Column;
+import com.gitee.sunchenbin.mybatis.actable.annotation.Table;
+import com.gitee.sunchenbin.mybatis.actable.command.BaseModel;
+import com.gitee.sunchenbin.mybatis.actable.constants.MySqlTypeConstant;
+
+import java.sql.Date;
+
+/**
+ * 第一种定义方式，使用@Table和@Column方式定义
+ *
+ * @author 徐森威
+ * @date 2020/12/24
+ */
+@Table(name = "test")
+public class Test extends BaseModel {
+
+	@Column(name = "id",type = MySqlTypeConstant.INT,length = 11,isKey = true,isAutoIncrement = true)
+	private Integer	id;
+
+	@Column(name = "name",type = MySqlTypeConstant.VARCHAR, length = 45, defaultValue = "hello")
+	private String	name;
+
+	@Column(name = "type",type = MySqlTypeConstant.VARCHAR,length = 45)
+	private String	type;
+
+	@Column(name = "description",type = MySqlTypeConstant.TEXT)
+	private String	description;
+
+	@Column(name = "create_time",type = MySqlTypeConstant.DATETIME)
+	private Date	create_time;
+
+	@Column(name = "update_time",type = MySqlTypeConstant.DATETIME)
+	private Date	update_time;
+
+	@Column(name = "number",type = MySqlTypeConstant.BIGINT,length = 5)
+	private Long	number;
+
+	@Column(name = "lifecycle",type = MySqlTypeConstant.CHAR,length = 1)
+	private String	lifecycle;
+
+	@Column(name = "dekes",type = MySqlTypeConstant.DOUBLE,length = 5,decimalLength = 2)
+	private Double	dekes;
+
+	public Integer getId(){
+		return id;
+	}
+
+	public void setId(Integer id){
+		this.id = id;
+	}
+
+	public String getName(){
+		return name;
+	}
+
+	public void setName(String name){
+		this.name = name;
+	}
+
+	public Date getCreate_time(){
+		return create_time;
+	}
+
+	public void setCreate_time(Date create_time){
+		this.create_time = create_time;
+	}
+
+	 public Date getUpdate_time(){
+	 return update_time;
+	 }
+	
+	 public void setUpdate_time(Date update_time){
+	 this.update_time = update_time;
+	 }
+
+	public String getDescription(){
+		return description;
+	}
+
+	public void setDescription(String description){
+		this.description = description;
+	}
+
+	public Long getNumber(){
+		return number;
+	}
+
+	public void setNumber(Long number){
+		this.number = number;
+	}
+
+	public String getLifecycle(){
+		return lifecycle;
+	}
+
+	public void setLifecycle(String lifecycle){
+		this.lifecycle = lifecycle;
+	}
+
+	public Double getDekes(){
+		return dekes;
+	}
+
+	public void setDekes(Double dekes){
+		this.dekes = dekes;
+	}
+
+	public String getType() {
+		return this.type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+}
